93fc7c0999f5b82a55f79d1e673d36128961d5a1
HNY_Badger <markelov-02@list.ru>
2024-03-18 18:10:26 +0400

    feat: finish queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -168,8 +168,20 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return new Promise((res) => {
+    let count = 0;
+    const arr = [];
+    promises.forEach((prom, i) => {
+      prom.then((val) => {
+        count += 1;
+        arr[i] = val;
+        if (count === promises.length) {
+          res(arr.join(''));
+        }
+      });
+    });
+  });
 }
 
 module.exports = {
